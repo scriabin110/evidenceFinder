7050b34ee03bd5dd96dbfe85a061ae6a15fada6f
Ito Keisuke <ito.keisuke3@jgc.com>
2024-07-31 07:25:34 +0900

    max_tokens = 0

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets['openAI_api_id']    #'YOUR_OPENAI_API_
 os.environ["GOOGLE_CSE_ID"] = st.secrets['cse_id']    #'YOUR_CSE_ID'
 os.environ["GOOGLE_API_KEY"] = st.secrets['Google_api_key']    #'YOUR_GOOGLE_API_KEY'
 
-llm = ChatOpenAI(model_name="gpt-4", temperature=0.75)
+llm = ChatOpenAI(model_name="gpt-4", max_tokens=None, temperature=0.75)
 
 template_0 = """
 # 命令書
